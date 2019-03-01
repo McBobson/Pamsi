5c6b908853a23959338e3745b41a52988706068f
McBobson <maciej.balcer.98@gmail.com>
2019-03-01 16:07:46 +0100

    Testowy Commit
    Coś tu musi być

---
+++ Test.cpp
@@ -0,0 +1,39 @@
+﻿// Test.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!\n"; 
+
+   int tab[100];
+   int tab1[100];
+
+   for (int i = 0; i < 100; i++)
+   {
+	   tab[i] = tab1[100];
+	   for (int j = 0; j < 100 ; j++)
+	   {
+		   int kek = rand();
+		   tab1[i] = kek;
+		   cout << tab1[i] << " ";
+	   }
+	   cout << endl;
+   }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
