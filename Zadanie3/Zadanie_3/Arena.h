46985669d89ce3c2fedfba0cb8bd773237bd260b
McBobson <maciej.balcer.98@gmail.com>
2019-04-27 18:06:40 +0200

    Dzialajce warcaby dla 2 graczy

--- Zadanie3/Zadanie_3/Arena.h
+++ Zadanie3/Zadanie_3/Arena.h
@@ -9,16 +9,20 @@ class Arena{
 public:
 
 	char **tablica;
+	int **wagi;
 	int pom[2];
+	
 
 	Arena();
 	~Arena();
 
-	void Przesun_Pionek(int,int,int,int);
+	void Przesun_Pionek(int,int,int,int,int);
 	bool Czy_Jest_Pionek(int,int);
 	bool Czy_Jest_W_Arenie(int,int);
 	int Kierunek_Bicia(int[]);
 	bool Bicie(int, int,int[]);
+	void Zamien(char, int, int);
+	bool Czy_Prawidlowy_Ruch(int[],char);
 
 
 
@@ -27,10 +31,12 @@ public:
 Arena::Arena()
 {
 	tablica = new char *[ROZ];
+	wagi = new int *[ROZ];
 
 	for (int i = 0; i < ROZ; i++)
 	{
 		tablica[i] = new char[ROZ];
+		wagi[i] = new int[ROZ];
 	}
 
 	for (int i = 0; i < ROZ; i++)
@@ -38,6 +44,7 @@ Arena::Arena()
 		for (int j = 0; j < ROZ; j++)
 		{
 			tablica[i][j] = ' ';
+			wagi[i][j] = 0;
 		}
 	}
 
@@ -46,12 +53,14 @@ Arena::Arena()
 		for (int j = 0; j < 8; j+=2)
 		{
 			tablica[i][j] = 'X';
+			wagi[i][j] = 1;
 		}
 	}
 
 	for (int i = 1; i < 8; i+=2)
 	{
 		tablica[1][i] = 'X';
+		wagi[1][i] = 1;
 	}
 
 	
@@ -60,15 +69,15 @@ Arena::Arena()
 		for (int j = 1; j < 8; j += 2)
 		{
 			tablica[i][j] = 'O';
+			wagi[i][j] = 1;
 		}
 	}
 
 	for (int i = 0; i < 8; i += 2)
 	{
 		tablica[6][i] = 'O';
+		wagi[6][i] = 1;
 	}
-
-	
 
 
 }
@@ -78,9 +87,11 @@ Arena::~Arena()
 	for (int i = 0; i < ROZ; i++)
 	{
 		delete[] tablica[i];
+		delete[] wagi[i];
 	}
 
 	delete[] tablica;
+	delete[] wagi;
 }
 
 bool Arena::Czy_Jest_Pionek(int x, int y)
@@ -125,40 +136,7 @@ int Arena::Kierunek_Bicia(int tab[])
 }
 
 bool Arena::Bicie(int x, int y, int tab[])
-{
-	/*
-	if (tab[0] < 0 && tab[1] < 0)
-	{
-		if (!Czy_Jest_Pionek(x - 1, y - 1) && Czy_Jest_W_Arenie(x-1,y-1))
-		{
-			return true;
-		}
-	}
-	else if (tab[0] < 0 && tab[1] > 0)
-	{
-		if (!Czy_Jest_Pionek(x - 1, y + 1) && Czy_Jest_W_Arenie(x - 1, y + 1))
-		{
-			return true;
-		}
-	}
-	else if (tab[0] > 0 && tab[1] < 0)
-	{
-		if (!Czy_Jest_Pionek(x + 1, y - 1) && Czy_Jest_W_Arenie(x + 1, y - 1))
-		{
-			return true;
-		}
-	}
-	else if (tab[0] < 0 && tab[1] < 0)
-	{
-		if (!Czy_Jest_Pionek(x + 1, y + 1) && Czy_Jest_W_Arenie(x + 1, y + 1))
-		{
-			return true;
-		}
-	}
-	else return false;
-	*/
-
-	
+{	
 	switch (Kierunek_Bicia(tab))
 	{
 	case 0:
@@ -206,70 +184,145 @@ bool Arena::Bicie(int x, int y, int tab[])
 	
 }
 
-void Arena::Przesun_Pionek(int y1,int x1,int y2,int x2)
+void Arena::Zamien(char znak, int x, int y)
 {
-	if (Czy_Jest_Pionek(x1, y1) && Czy_Jest_W_Arenie(x2, y2))
+	if (znak == 'X' && x == 7)
 	{
-		pom[0] = x2 - x1;
-		pom[1] = y2 - y1;
-		if (Czy_Jest_Pionek(x2, y2))
-		{
-			if (Bicie(x2, y2, pom))
-			{
-				switch (Kierunek_Bicia(pom))
-				{
-				case 0:
-				{
-					tablica[x2-1][y2-1] = tablica[x1][y1];
-					tablica[x1][y1] = ' ';
-					tablica[x2][y2] = ' ';
-				}
-				break;
-
-				case 1:
-				{
-					tablica[x2-1][y2+1] = tablica[x1][y1];
-					tablica[x1][y1] = ' ';
-					tablica[x2][y2] = ' ';
-				}
-				break;
-
-				case 2:
-				{
-					tablica[x2+1][y2-1] = tablica[x1][y1];
-					tablica[x1][y1] = ' ';
-					tablica[x2][y2] = ' ';
-				}
-				break;
-
-				case 3:
-				{
-					tablica[x2+1][y2+1] = tablica[x1][y1];
-					tablica[x1][y1] = ' ';
-					tablica[x2][y2] = ' ';
-				}
-				break;
-				}
-			}
-			else
-				cout << "Bicie nie jest mozliwe" << endl;
-		}
-		else
-		{
-			tablica[x2][y2] = tablica[x1][y1];
-			tablica[x1][y1] = ' ';
-		}
+		tablica[x][y] = 'C';
+		wagi[x][y] = 2;
 	}
-	else
+
+	if (znak == 'O' && x == 0)
 	{
-		cout << "Zjebales" << endl;
-		cout << "Kody b³edu: " << endl;
-		cout << "Czy jest pionek: " << Czy_Jest_Pionek(x1, y1) << endl;
-		cout << "Czy jest w arenie: " << Czy_Jest_W_Arenie(x2, y2) << endl;
+		tablica[x][y] = 'P';
+		wagi[x][y] = 2;
 	}
 }
 
+bool Arena::Czy_Prawidlowy_Ruch(int pom[],char znak)
+{
+	if ((abs(pom[0]) == 1 && abs(pom[1]) == 1 && (znak=='X' || znak=='O')) || ((abs(pom[0]) > 0 && abs(pom[1]) > 0 && (znak == 'P' || znak == 'C'))))
+	{
+		return true;
+	}
+	else return false;
+}
+
+void Arena::Przesun_Pionek(int y1,int x1,int y2,int x2, int gracz)
+{
+	pom[0] = x2 - x1;
+	pom[1] = y2 - y1;
+
+	if ((tablica[x1][y1] == 'O' && gracz == 1) || (tablica[x1][y1] == 'X' && gracz == 2))
+	{
+		if (Czy_Jest_Pionek(x1, y1) && Czy_Jest_W_Arenie(x2, y2) && Czy_Prawidlowy_Ruch(pom, tablica[x1][y1]))
+		{
+
+			if (Czy_Jest_Pionek(x2, y2))
+			{
+				if (Bicie(x2, y2, pom))
+				{
+					switch (Kierunek_Bicia(pom))
+					{
+					case 0:
+					{
+						tablica[x2 - 1][y2 - 1] = tablica[x1][y1];
+						tablica[x1][y1] = ' ';
+						tablica[x2][y2] = ' ';
+
+						wagi[x2 - 1][y2 - 1] = wagi[x1][y1];
+						wagi[x1][y1] = 0;
+						wagi[x2][y2] = 0;
+
+						Zamien(tablica[x2 - 1][y2 - 1], x2 - 1, y2 - 1);
+					}
+					break;
+
+					case 1:
+					{
+						tablica[x2 - 1][y2 + 1] = tablica[x1][y1];
+						tablica[x1][y1] = ' ';
+						tablica[x2][y2] = ' ';
+
+						wagi[x2 - 1][y2 + 1] = wagi[x1][y1];
+						wagi[x1][y1] = 0;
+						wagi[x2][y2] = 0;
+
+						Zamien(tablica[x2 - 1][y2 + 1], x2 - 1, y2 + 1);
+					}
+					break;
+
+					case 2:
+					{
+						tablica[x2 + 1][y2 - 1] = tablica[x1][y1];
+						tablica[x1][y1] = ' ';
+						tablica[x2][y2] = ' ';
+
+						wagi[x2 + 1][y2 - 1] = wagi[x1][y1];
+						wagi[x1][y1] = 0;
+						wagi[x2][y2] = 0;
+
+						Zamien(tablica[x2 + 1][y2 - 1], x2 + 1, y2 - 1);
+					}
+					break;
+
+					case 3:
+					{
+						tablica[x2 + 1][y2 + 1] = tablica[x1][y1];
+						tablica[x1][y1] = ' ';
+						tablica[x2][y2] = ' ';
+
+						wagi[x2 + 1][y2 + 1] = wagi[x1][y1];
+						wagi[x1][y1] = 0;
+						wagi[x2][y2] = 0;
+
+						Zamien(tablica[x2 + 1][y2 + 1], x2 + 1, y2 + 1);
+					}
+					break;
+					}
+				}
+				else
+					cout << "Bicie nie jest mozliwe" << endl;
+			}
+			else
+			{
+				tablica[x2][y2] = tablica[x1][y1];
+				tablica[x1][y1] = ' ';
+
+				wagi[x2][y2] = wagi[x1][y1];
+				wagi[x1][y1] = 0;
+
+				Zamien(tablica[x2][y2], x2, y2);
+			}
+
+		}
+		else
+		{
+			cout << "Zjebales" << endl;
+			cout << "Kody b³edu: " << endl;
+			cout << "Czy jest pionek: " << Czy_Jest_Pionek(x1, y1) << endl;
+			cout << "Czy jest w arenie: " << Czy_Jest_W_Arenie(x2, y2) << endl;
+			cout << "Czy prawidlowy ruch: " << Czy_Prawidlowy_Ruch(pom, tablica[x1][y1]) << endl;
+		}
+	}
+	else cout << "Trzymaj sie swoich pionow rycerzu!!!!" << endl;
+}
+
 ostream& operator << (ostream& wyjscie, Arena& A) {
+
+
+	{
+		wyjscie << endl << "       ";
+
+
+		for (int i = 65; i < 65 + 8; i++)
+		{
+			wyjscie << (char)i << "     ";
+		}
+
+		wyjscie << endl;
+
+	}
 
 	{
 
@@ -294,7 +347,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  "<< A.tablica[0][i] <<"  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "0";
 
 		wyjscie << endl << "    ";
 
@@ -323,7 +376,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 		wyjscie << "|  " << A.tablica[1][i] << "  ";
 	}
 
-	wyjscie << "|";
+	wyjscie << "|  "<<"1";
 
 	wyjscie << endl << "    ";
 
@@ -352,7 +405,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  " << A.tablica[2][i] << "  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "2";
 
 		wyjscie << endl << "    ";
 
@@ -381,7 +434,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  " << A.tablica[3][i] << "  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "3";
 
 		wyjscie << endl << "    ";
 
@@ -410,7 +463,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  " << A.tablica[4][i] << "  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "4";
 
 		wyjscie << endl << "    ";
 
@@ -439,7 +492,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  " << A.tablica[5][i] << "  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "5";
 
 		wyjscie << endl << "    ";
 
@@ -468,7 +521,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  " << A.tablica[6][i] << "  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "6";
 
 		wyjscie << endl << "    ";
 
@@ -496,7 +549,7 @@ ostream& operator << (ostream& wyjscie, Arena& A) {
 			wyjscie << "|  " << A.tablica[7][i] << "  ";
 		}
 
-		wyjscie << "|";
+		wyjscie << "|  " << "7";
 
 		wyjscie << endl << "    ";
 
--- Zadanie3/Zadanie_3/Zadanie_3.cpp
+++ Zadanie3/Zadanie_3/Zadanie_3.cpp
@@ -11,12 +11,21 @@ int main()
 
 	cout << A;
 
+	char warcab[2] = { 'O','X' };
+
+	int gracz;
 	int q = 0;
+	cout << endl << "Gracz 1 -> O" << endl << "Gracz 2 -> X" << endl;
+	cout << "Wybierz swojego championa: ";
+	cin >> gracz;
+
 	while(q!=1)
 	{
 		system("cls");
 
 		cout << A << endl;
+
+		cout << "TURA GRACZA " << gracz << " -> " << warcab[gracz - 1] << endl;
 
 		cout << "PODAJ POLOZENIE PIONKA DO PRZESUNIECIA I TO GDZIE CHCESZ GO PRZESUNAC" << endl;
 
@@ -25,11 +34,29 @@ int main()
 
 		cin >> a >> y1 >> b >> y2;
 
-		A.Przesun_Pionek((int)a - 65 ,y1 , (int)b - 65, y2);
+		A.Przesun_Pionek((int)a - 65 ,y1 , (int)b - 65, y2, gracz);
 
 		system("Pause");
 		system("cls");
 		cout << A;
+		
+		if (gracz == 2) gracz--;
+		else gracz++;
+
+		cout << endl;
+
+		cout << "TURA GRACZA " << gracz <<" -> " << warcab[gracz-1] << endl;
+
+		cout << "PODAJ POLOZENIE PIONKA DO PRZESUNIECIA I TO GDZIE CHCESZ GO PRZESUNAC" << endl;
+
+		cin >> a >> y1 >> b >> y2;
+
+		A.Przesun_Pionek((int)a - 65, y1, (int)b - 65, y2, gracz);
+
+		system("Pause");
+		system("cls");
+		cout << A;
+		gracz--;
 	}
 	
 
